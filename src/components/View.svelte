2d32ce0559933ba5f805de2cb7675eddd1fd9d91
SomMariya <m.som-2030@kpi.ua>
2021-04-04 19:23:33 +0300

    Updated book view-window

--- src/components/View.svelte
+++ src/components/View.svelte
@@ -14,7 +14,6 @@
   height: 87%;
   padding: 1rem;
   margin: 2% auto;
-  text-align: center;
   border-radius: 50px;
 }
 .close {
@@ -22,21 +21,65 @@
   float: right;
   cursor: pointer;
   margin-right: 20px;
+  font-size: 2em;
 }
 .close:hover {
   font-weight: bold;
 }
+.viewContainer {
+  display: grid;
+  grid-template-columns: 0.5fr 1.5fr;
+}
 #comment {
   background-color: brown;
-  width: 40%;
-  height: 15vw;
+  width: 50%;
+  height: 20vw;
+}
+#bookImg {
+  height: 18vw;
+  width: 14vw;
+  margin-top: 9%;
+}
+#title {
+  font-size: 1.3em;
+  margin-left: 3%;
+}
+#previewLink {
+  color: darkblue;
+  text-decoration: none;
+}
+#previewLink:hover {
+  color: indigo;
+  text-decoration: underline;
+}
+#bookinfo ul {
+  list-style-type: none;
+  text-align: left;
+  padding-left: 0;
+  width: 80%;
+}
+#description {
+  width: 55vw;
+  height: 5vw;
+  resize: none;
 }
 </style>
 
 <script>
 export let wholeinfo;
 const { volumeInfo } = wholeinfo;
-const { title, imageLinks, authors } = volumeInfo;
+const {
+  title,
+  imageLinks,
+  authors,
+  publishedDate,
+  categories,
+  description,
+  language,
+  pageCount,
+  previewLink,
+} = volumeInfo;
+import fileIcon from '../../static/fileIcon.png';
 let thumbnailUrl;
 
 if (imageLinks) {
@@ -59,9 +102,46 @@ export function hide() {
   <div class="view-wrapper">
     <div class="view">
       <span class="close" on:click={() => hide()}>&times;</span>
-      <div>{title}</div>
-      <img src={thumbnailUrl} alt="cover" />
-      <div id="comment" />
+      <div id="title"><b>"{title}"</b></div>
+      <div class="viewContainer">
+        <img id="bookImg" src={thumbnailUrl} alt="cover" />
+        <div id="bookinfo">
+          <ul>
+            <li>
+              <b>Authors: </b>
+              {#if authors == undefined}
+                Unknown
+              {:else}
+                {authors}
+              {/if}
+            </li>
+            <li><b>Published Date: </b>{publishedDate}</li>
+            <li>
+              <b>Description: </b>
+              {#if description}
+                <textarea id="description">{description}</textarea>
+              {:else}
+                Unknown
+              {/if}
+            </li>
+            <li><b>Categories: </b>"{categories}"</li>
+            <li><b>Language: </b>"{language}"</li>
+            <li>
+              <b>PageCount: </b>
+              {#if pageCount == undefined}
+                Unknown
+              {:else}
+                {pageCount}
+              {/if}
+            </li>
+            <li>
+              <b>Book preview: </b><a id="previewLink" href={previewLink}
+                >link</a>
+            </li>
+          </ul>
+        </div>
+        <div id="comment" />
+      </div>
       <slot />
     </div>
   </div>
